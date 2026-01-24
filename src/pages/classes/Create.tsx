3ecfbbb1d3e905a57d027506c0f30c9ee46bb2e0
skrsikop <skrsikop@gmail.com>
2026-01-24 10:51:45 +0300

    error up to date

--- src/pages/classes/Create.tsx
+++ src/pages/classes/Create.tsx
@@ -5,7 +5,7 @@ import {useBack} from "@refinedev/core";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { appendErrors, useForm } from "react-hook-form"
+import {  useForm } from "@refinedev/react-hook-form"
 import {classSchema} from "@/lib/schema.ts";
 import * as z from "zod";
 
